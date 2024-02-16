824f5aa733082b4e207c5a78f4255d717f7007dd
ShaYri12 <sharjeelb60@gmail.com>
2024-02-16 16:31:06 +0500

    fixing vercel

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT ||  8000
 const corsOption ={
-    origin: process.env.ALLOWED_ORIGIN || "https://tour-management-htux.vercel.app/",
+    origin: process.env.ALLOWED_ORIGIN || "https://tour-management-livid.vercel.app/api/tours",
     credentials: true,
 }
 
