e6b168b9184b7098c9a1bde338c80fe334cff80c
ShaYri12 <sharjeelb60@gmail.com>
2024-02-17 02:05:16 +0500

    ve

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,14 @@ app.use(cors(corsOption))
 app.use(cookieParser())
 app.use('/api/auth', authRoute)
 app.use('/api/tours', tourRoute)
+// Handle preflight requests
+app.options('/api/tours/*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(200).end();
+});
+
 app.use('/api/users', userRoute)
 app.use('/api/review', reviewRoute)
 app.use('/api/booking', bookingRoute)
